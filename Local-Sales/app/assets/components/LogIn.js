4db9e66d4b21fe27a2eb39d81448185327fd60a7
ElenaGospodinova <gospodinovaelena@yahoo.co.uk>
2023-10-12 17:51:19 +0100

    Validation Yup added

--- Local-Sales/app/assets/components/LogIn.js
+++ Local-Sales/app/assets/components/LogIn.js
@@ -3,24 +3,32 @@ import { View, TextInput, StyleSheet, Text, Image, Alert } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { Formik } from 'formik';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
 
 import Screen from './Screen';
 import colors from '../config/colors';
 import defaultStyles from '../config/styles';
 import AppButton from './AppButton';
 
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password:Yup.string().required().min(4).matches().label('Password')
+});
+
 export default function InputText({ icon, ...otherProps }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const navigation = useNavigation();
+
   const handleLogIn = () => {
        if (!email || !password) {
        
         Alert.alert('Welcome to Local Sales App');
         navigation.navigate('Account');
       } else {
-         console.log('Navigating to Items screen');
+         Alert.alert('Error in email or/and password');
       
        }
     };
@@ -33,50 +41,52 @@ export default function InputText({ icon, ...otherProps }) {
       </View>
       <Formik 
           initialValues={{ email:'', password:''}}
-          onSubmit={values => console.log(values)}
+          onSubmit={(values) => console.log(values)}
+          validationSchema={validationSchema}
           >
-            { ({ handleChange, handleSubmit}) => (
+            { ({ handleChange, handleSubmit, errors }) => (
               <>
               <View style={styles.container}>
-          <FontAwesome name='envelope' size={24} color={colors.green} style={styles.icon} />
+                <FontAwesome name='envelope' size={24} color={colors.green} style={styles.icon} />
+                
+                <TextInput
+                  autoCapitalize='none'
+                  autoCorrect={false}
+                  style={styles.textInput}
+                  onChangeText={handleChange('email')}
+                  keyboardType='default'
+                  icon='lock'
+                  maxLength={45}
+                  placeholder="Email"
+                  clearButtonMode='always'
+                  {...otherProps}
+                />
+              </View>
+              <Text style={styles.errors}>{errors.email}</Text>
+              <View style={styles.container}>
+                <FontAwesome name="lock" size={24} color={colors.green} style={styles.icon} />
 
-          <TextInput
-            autoCapitalize='none'
-            autoCorrect={false}
-            style={styles.textInput}
-            onChangeText={handleChange('email')}
-            keyboardType='default'
-            icon='lock'
-            maxLength={45}
-            placeholder="Email"
-            clearButtonMode='always'
-            {...otherProps}
-          />
-        </View>
-
-        <View style={styles.container}>
-          <FontAwesome name="lock" size={24} color={colors.green} style={styles.icon} />
-
-          <TextInput
-            autoCapitalize='none'
-            autoCorrect={false}
-            style={defaultStyles.textLogIn}
-            onChangeText={(handleChange('password'))}
-            keyboardType='email-address'
-            maxLength={45}
-            placeholder="Password"
-            secureTextEntry={true}
-            clearButtonMode='always'
-            {...otherProps}
-          />
-        </View>
-        <AppButton  style={styles.btn}
-            title="LogIn" 
-            onPress={() => {
-                handleSubmit();
-                handleLogIn();
-              }}
-            />
+                <TextInput
+                  autoCapitalize='none'
+                  autoCorrect={false}
+                  style={defaultStyles.textLogIn}
+                  onChangeText={(handleChange('password'))}
+                  keyboardType='email-address'
+                  maxLength={45}
+                  placeholder="Password"
+                  secureTextEntry={true}
+                  clearButtonMode='always'
+                  {...otherProps}
+                />
+              </View>
+              <Text style={styles.errors}>{errors.password}</Text>
+              <AppButton  style={styles.btn}
+                  title="LogIn" 
+                  onPress={() => {
+                      handleSubmit();
+                      handleLogIn();
+                    }}
+                  />
 
               </>
             )}
@@ -101,6 +111,10 @@ const styles = StyleSheet.create({
     padding: 15,
     marginVertical: 5,
     marginTop: 42,
+  },
+  errors:{
+    color:'red',
+    marginLeft:42,
   },
   info: {
     marginTop: 22,
